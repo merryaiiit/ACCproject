7802163476b860896ff4e82c6ca651c0b97f7185
sariel <li.ju.0122@student.uu.se>
2020-10-16 13:06:04 +0200

    NFS

--- docker/shared/project/tasks.py
+++ docker/shared/project/tasks.py
@@ -5,6 +5,6 @@ import os
 @app.task
 def analyze(filename):
     call("cd ./murtazo/navier_stokes_solver/ && ./airfoil 10 0.0001 10. 1 ../cloudnaca/msh/" + filename, shell=True)
-    # call("scp ../murtazo/navier_stokes_solver/results/drag_ligt.m ubuntu@"+os.environ['BASEIP']+":/home/ubuntu/.")
+    call("cp ./murtazo/navier_stokes_solver/results/drag_ligt.m /results/"+filename+"result.m")
 
     return 0
