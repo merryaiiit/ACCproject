b5e3d30d274d800d24dc24f5db2152da0034363d
sariel <li.ju.0122@student.uu.se>
2020-10-17 11:16:45 +0200

    inside-container: novaclient & keystoneclient required

--- docker/shared/entry.sh
+++ docker/shared/entry.sh
@@ -1,2 +1,4 @@
 sed 's,to_be_replaced,'"${RMQIP}"',g' ./entry-cloud.txt | sudo tee ./worker.txt
+pip3 install python-novaclient
+pip3 install python-keystoneclient
 celery -A project worker & python3 -m project.run & sleep infinity
