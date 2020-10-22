6c0204fd3379e9aa5679916380acdc911ef59b66
sariel <li.ju.0122@student.uu.se>
2020-10-22 16:48:10 +0200

    run with os.system

--- docker/shared/project/tasks.py
+++ docker/shared/project/tasks.py
@@ -1,11 +1,10 @@
 from project.celery import app
-from subprocess import call
 import os
 
 @app.task
 def analyze(filename):
-    call("rm -f ./murtazo/navier_stokes_solver/results/*", shell=True)
-    call("cd ./murtazo/navier_stokes_solver/ && ./airfoil 10 0.0001 10. 1 /meshes/" + filename, shell=True)
-    call("cp ./results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m", shell=True)
+    os.system("rm -f ./murtazo/navier_stokes_solver/results/*")
+    os.system("cd ./murtazo/navier_stokes_solver/ && ./airfoil 10 0.0001 10. 1 /meshes/" + filename)
+    os.system("cp ./results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m")
 
     return 0
