d362351c8326c22ec781ec3b9377c56baa7cdd27
sariel <li.ju.0122@student.uu.se>
2020-10-22 12:49:28 +0200

    regenerating fixed

--- docker/shared/entry.sh
+++ docker/shared/entry.sh
@@ -1,2 +1,24 @@
+echo "hello world! "
+tar xzvf murtazo.tgz && cd murtazo
+tar xvf cloudnaca.tgz && tar xvf navier_stokes_solver.tar
+cd navier_stokes_solver/src && ./compile_forms
+cd .. && cmake . && make -j 2
+echo "Make --- finished"
+cd ../cloudnaca && sudo apt-get update && sudo dpkg --configure -a
+echo "apt-get updated"
+sudo apt-get install -y gmsh && sudo apt install -y python-numpy
+./runme.sh 0 30 10 200 2 && cd ./msh 
+
+sfx=".xml"
+for file in *
+do
+    name=${file%.msh}
+    echo "${file}"
+    echo "${name}${sfx}"
+    dolfin-convert "${file}" "${name}${sfx}"
+done
+
+cp ./* /meshes/
+
 sed 's,to_be_replaced,'"${RMQIP}"',g' ./worker-cloud.txt | sudo tee ./worker-cloud.txt
 celery -A project worker & python3 -m project.run & sleep infinity
--- docker/shared/worker.sh
+++ docker/shared/worker.sh
@@ -1,1 +1,8 @@
+echo "hello world! "
+tar xzvf murtazo.tgz && cd murtazo
+tar xvf navier_stokes_solver.tar
+cd navier_stokes_solver/src && ./compile_forms
+cd .. && cmake . && make -j 2
+echo "Make --- finished"
+
 celery -A project worker & sleep infinity
