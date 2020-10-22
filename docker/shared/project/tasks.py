9441f81cf9d4ba02474da14059678589c6a4c247
Li-Ju <55688441+Li-Ju666@users.noreply.github.com>
2020-10-22 17:10:54 +0200

    Update tasks.py

--- docker/shared/project/tasks.py
+++ docker/shared/project/tasks.py
@@ -5,6 +5,6 @@ import os
 def analyze(filename):
     os.system("rm -f ./murtazo/navier_stokes_solver/results/*")
     os.system("cd ./murtazo/navier_stokes_solver/ && ./airfoil 10 0.0001 10. 1 /meshes/" + filename)
-    os.system("cp ./results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m")
+    os.system("cp ./murtazo/navier_stokes_solver/results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m")
 
     return 0
