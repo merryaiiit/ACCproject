475e15b10474e4d2a7fe8f9d95776c349a65a1db
sariel <li.ju.0122@student.uu.se>
2020-10-22 15:11:14 +0200

    task bug fixed

--- docker/shared/project/tasks.py
+++ docker/shared/project/tasks.py
@@ -4,8 +4,8 @@ import os
 
 @app.task
 def analyze(filename):
-    call("rm -f ./murtazo/navier_stokes_solver/results/*")
+    call("rm -f ./murtazo/navier_stokes_solver/results/*", shell=True)
     call("cd ./murtazo/navier_stokes_solver/ && ./airfoil 10 0.0001 10. 1 /meshes/" + filename, shell=True)
-    call("cp ./results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m")
+    call("cp ./results/drag_ligt.m /results/"+filename.split(sep='.')[0]+"_result.m", shell=True)
 
     return 0
