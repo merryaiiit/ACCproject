db507f0a93987dcae407c472fd1f23f0720d92f0
sariel <li.ju.0122@student.uu.se>
2020-10-22 13:54:44 +0200

    permission_problem

--- docker/shared/entry.sh
+++ docker/shared/entry.sh
@@ -18,7 +18,7 @@ do
     dolfin-convert "${file}" "${name}${sfx}"
 done
 
-sudo cp ./* /meshes/
+cp ./* /meshes/
 cd /home/fenics/shared/
 sed 's,to_be_replaced,'"${RMQIP}"',g' ./worker-cloud.txt | sudo tee ./worker-cloud.txt
 celery -A project worker & python3 -m project.run & sleep infinity
