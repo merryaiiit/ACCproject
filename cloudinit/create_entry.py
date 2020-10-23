516f90b9f614ead2aec73649056dc0c6a5de6b5f
Aneysha <61636915+Aneysha@users.noreply.github.com>
2020-10-23 17:06:22 +0200

    Update create_entry.py

--- cloudinit/create_entry.py
+++ cloudinit/create_entry.py
@@ -16,19 +16,19 @@ image_name = "Ubuntu 18.04"
 
 loader = loading.get_plugin_loader('password')
 
+#auth = loader.load_from_options(auth_url="https://east-1.cloud.snic.se:5000/v3",
+#                                username="s16071",
+#                                password="Sariel199524",
+#                                project_name="UPPMAX 2020/1-2",
+#                                project_id="fc1aade83c2e49baa7498b3918560d9f",
+#                                user_domain_name="snic")
+
 auth = loader.load_from_options(auth_url="https://east-1.cloud.snic.se:5000/v3",
-                                username="s16071",
-                                password="Sariel199524",
+                                username="s17012",
+                                password="An3y$h@31287",
                                 project_name="UPPMAX 2020/1-2",
                                 project_id="fc1aade83c2e49baa7498b3918560d9f",
                                 user_domain_name="snic")
-
-# auth = loader.load_from_options(auth_url="https://east-1.cloud.snic.se:5000/v3",
-#                                 username="s17012",
-#                                 password="An3y$h@31287",
-#                                 project_name="UPPMAX 2020/1-2",
-#                                 project_id="fc1aade83c2e49baa7498b3918560d9f",
-#                                 user_domain_name="snic")
 sess = session.Session(auth=auth)
 nova = client.Client('2.1', session=sess)
 print("user authorization completed.")
@@ -54,7 +54,7 @@ else:
 secgroups = ['default']
 
 print("Creating instance ... ")
-instance = nova.servers.create(name="g10-entry", key_name="liju_wm", image=image, flavor=flavor, userdata=userdata, nics=nics,security_groups=secgroups)
+instance = nova.servers.create(name="g10-entry", key_name="Aneysha_test_instance", image=image, flavor=flavor, userdata=userdata, nics=nics,security_groups=secgroups)
 inst_status = instance.status
 print("waiting for 10 seconds.. ")
 time.sleep(10)
