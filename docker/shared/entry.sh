4800f8c1937a115147ed05fd521b90415eae7c51
Aneysha <61636915+Aneysha@users.noreply.github.com>
2020-10-23 11:12:40 +0200

    removed file processing to init.sh
    removed file processing to init.sh

--- docker/shared/entry.sh
+++ docker/shared/entry.sh
@@ -7,18 +7,6 @@ echo "Make --- finished"
 cd ../cloudnaca && sudo apt-get update && sudo dpkg --configure -a
 echo "apt-get updated"
 sudo apt-get install -y gmsh && sudo apt install -y python-numpy
-./runme.sh 0 30 10 200 2 && cd ./msh 
-
-sfx=".xml"
-for file in *
-do
-    name=${file%.msh}
-    echo "${file}"
-    echo "${name}${sfx}"
-    dolfin-convert "${file}" "${name}${sfx}"
-done
-
-cp ./* /meshes/
 cd /home/fenics/shared/
 sed 's,to_be_replaced,'"${RMQIP}"',g' ./worker-cloud.txt | sudo tee ./worker-cloud.txt
 celery -A project worker & python3 -m project.run & sleep infinity
